669a8cb9871e058c6080e9526dd3114704ef1a2c
M.H-H <Melanie.Holzapfel-Huettenrauch@Student.HTW-Berlin.de>
2021-04-12 13:49:26 +0200

    Lesson5_handle-incoming-data

--- main.js
+++ main.js
@@ -1,20 +1,39 @@
-const http = require("http");
-const httpStatus = require("http-status-codes");
-const port = 3000;
+"use strict";
 
-let responseMessage = "<h1>Hello, Universe!</h1>";
+const   http = require("http"),
+        httpStatus = require("http-status-codes"),
+        port = 3000,
+        app = http.createServer();
 
-const app = http.createServer((req, res) => {
+let responseMessage = "<h1>This will be shown</h1>";
+
+const getJSONString = obj => {
+    return JSON.stringify(obj, null, 2);
+}
+
+app.on("request", (req, res) => {
+
+    var body = [];
+    
+    req.on("data", (bodyData) => {
+        body.push(bodyData);
+    });
+
+    req.on("end", () => {
+        body = Buffer.concat(body).toString();
+        console.log(`Request´s body content: ${body}`);
+    });
 
     console.log("Received an incoming request!");
+    console.log(`Method: ${getJSONString(req.method)}`);
+    console.log(`Url: ${getJSONString(req.url)}`);
+    console.log(`Headers: ${getJSONString(req.headers)}`);
 
     res.writeHead(200, {
         "Content-Type": "text/html"
     });
 
-    res.write(responseMessage);
-
-    res.end();
+    res.end(responseMessage);
 
     console.log(`Sent a response : ${responseMessage}`);
 });
