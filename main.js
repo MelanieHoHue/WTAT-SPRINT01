cd18c70683eb37f9a559b3527991704605558a76
M.H-H <Melanie.Holzapfel-Huettenrauch@Student.HTW-Berlin.de>
2021-04-12 16:49:28 +0200

    Lesson5_simple-routing

--- main.js
+++ main.js
@@ -3,9 +3,16 @@
 const   http = require("http"),
         httpStatus = require("http-status-codes"),
         port = 3000,
-        app = http.createServer();
+        app = http.createServer(),
+        routeResponseMap = {
+            "/info": "<h1>Info Page</h1>",
+            "/contact": "<h1>Contact Us</h1>",
+            "/about": "<h1>Learn More About Us.</h1>",
+            "/hello": "<h1>Say hello by emailing us here</h1>",
+            "/error": "<h1>Sorry the page you are looking for is not here.</h1>"
+        };
 
-let responseMessage = "<h1>This will be shown</h1>";
+let responseMessage = "";
 
 const getJSONString = obj => {
     return JSON.stringify(obj, null, 2);
@@ -13,29 +20,17 @@ const getJSONString = obj => {
 
 app.on("request", (req, res) => {
 
-    var body = [];
-    
-    req.on("data", (bodyData) => {
-        body.push(bodyData);
-    });
-
-    req.on("end", () => {
-        body = Buffer.concat(body).toString();
-        console.log(`Request´s body content: ${body}`);
-    });
-
-    console.log("Received an incoming request!");
-    console.log(`Method: ${getJSONString(req.method)}`);
-    console.log(`Url: ${getJSONString(req.url)}`);
-    console.log(`Headers: ${getJSONString(req.headers)}`);
-
     res.writeHead(200, {
         "Content-Type": "text/html"
     });
 
-    res.end(responseMessage);
+    if(routeResponseMap[req.url]) {
+        responseMessage = routeResponseMap[req.url]
+    } else {
+        responseMessage = "<h1>Welcome!</h1>";
+    }
 
-    console.log(`Sent a response : ${responseMessage}`);
+    res.end(responseMessage), 20000
 });
 
 app.listen(port);
